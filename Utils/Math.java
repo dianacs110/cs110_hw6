75cb84ba4740b3b8c190acfa4519adf123326c16
dianacs110 <diana.gevorgyan16@gmail.com>
2016-11-27 20:03:45 +0400

    Update Math.java

--- Utils/Math.java
+++ Utils/Math.java
@@ -1,15 +1,12 @@
 package Utils;
 public class Math{
- 
-
-public static int factorial(final int n){
+ public static int factorial(final int n){
     if (n==0){
         return 1;
     }
     return n*factorial(n-1);
-    
-    
-}
+    }
+ 
  public static int factorial1(int n){
         int f = 1;
         for(int i = 1; i<=n; i++){
