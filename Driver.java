37b250df29b88635a2d5b2cf768e5344ffe2faa7
unknown <diana.gevorgyan@gmail.com>
2016-11-27 19:59:24 +0400

    first commit

---
+++ Driver.java
@@ -0,0 +1,13 @@
+import Shapes.Square;
+import Shapes.Rectangle;
+import Utils.Math;
+public class Driver{
+public static void main(String[] args){
+Square area = new Square(10);
+Rectangle recarea = new Rectangle(2,50);
+System.out.println(area.getArea());
+System.out.println(recarea.getArea());
+System.out.println(Math.factorial((int)16));
+System.out.println(Math.factorial1((int)10.6));
+}
+}
